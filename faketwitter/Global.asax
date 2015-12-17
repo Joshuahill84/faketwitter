11c81170e06aa590d52cddedbc43fa6c26fdd64b
Josh Hill <joshuahill84@gmail.com>
2015-12-17 14:03:11 -0600

    initial commit

---
+++ faketwitter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="faketwitter.MvcApplication" Language="C#" %>
---
+++ faketwitter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(faketwitter.Startup))]
+namespace faketwitter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
