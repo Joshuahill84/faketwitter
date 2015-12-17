b412ccc986471bc4fe5f3197f61147cadae6804c
Josh Hill <joshuahill84@gmail.com>
2015-12-17 16:34:23 -0600

    added class user,follower,post

---
+++ faketwitter/Models/Follower.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace faketwitter.Models
+{
+    public class Follower
+    {
+        public List<User> Users { get; set; }
+    }
+}
